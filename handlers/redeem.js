acd5097d2ae8c80c1e3e4a8608d94d49c63b1006
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-01-24 22:22:46 -0700

    Update redeem.js

--- handlers/redeem.js
+++ handlers/redeem.js
@@ -5,17 +5,6 @@ let modID = '698614112';
 let timeoutID = '886b8287-70fe-4e54-a071-aadd5a4922a8'
 
 async function redeem(client, eventData) {
-    if (rewardType === timeoutID) {
-        let streamerID = await func.getUserID(channel);
-
-        let user = message.replace('@', '');
-        let userID = await func.getUserID(user);
-
-        let timeout = await func.timeoutUser(streamerID, userID, modID, 300, 'Por canje de puntos');
-
-        client.say(channel, `${tags['display-name']} ha fusilado a ${user} por 5 min.`);
-    }
-
     console.log(eventData);
 }
 
