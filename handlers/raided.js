830d214617edfd4bae1744ca68b628588f7658f4
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-01-25 22:38:44 -0700

    Update raided.js
    Now it doesn't stop without saying thank you for the raid

--- handlers/raided.js
+++ handlers/raided.js
@@ -6,7 +6,7 @@ async function raid(client, channel, username, viewers, tags) {
     if (viewers < 2) return;
     let { soClip } = await shoutout(channel, username, modID);
     if (!soClip) {
-        return client.say(channel, `No se ha podido reproducir el clip!`);
+        client.say(channel, `No se ha podido reproducir el clip!`);
     }
 
     client.say(channel, `Gracias por la raid de ${viewers} person${viewers > 1 ? 'a' : 'itas'} ${username}!`);
