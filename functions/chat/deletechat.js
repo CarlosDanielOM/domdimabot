eb165635e6d73de271e9411bc37661b819b0a6d5
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-01-28 02:31:41 -0700

    Fixed
    Fixed the Helix URL request for message deletion

--- functions/chat/deletechat.js
+++ functions/chat/deletechat.js
@@ -1,14 +1,15 @@
 async function clearChat() {
-    let response = await fetch(`${this.helixURL}/chat?broadcaster_id=${this.userID}&moderator_id=${this.modID}`, {
+    let response = await fetch(`${this.helixURL}/moderation/chat?broadcaster_id=${this.userID}&moderator_id=${this.modID}`, {
         method: 'DELETE',
         headers: this.streamerHeaders
     })
+
+    if (response.status === 204) return { error: false, status: 204 };
 
     let json = await response.json();
 
     if (json.error) return json;
 
-    if (json.status === 204) return { error: false, status: 204 };
 }
 
 module.exports = clearChat;
