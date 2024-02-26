b760bbbe58d4d81af061b854321072d19277a65f
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-02-26 00:48:22 -0700

    Circular Dependecy Fix

--- handlers/redeem.js
+++ handlers/redeem.js
@@ -2,14 +2,13 @@ const func = require('../functions');
 const triggerSchema = require('../schemas/trigger');
 const triggerFileSchema = require('../schemas/triggerfile');
 
-const { sendTrigger } = require('../src/server');
-
 let modID = '698614112';
 
 let timeoutID = '886b8287-70fe-4e54-a071-aadd5a4922a8'
 
 async function redeem(client, eventData) {
     console.log('redeem')
+    const { sendTrigger } = require('../src/server');
     const { broadcaster_user_id, broadcaster_user_login, user_id, user_login, user_input } = eventData;
     const { reward } = eventData;
 
