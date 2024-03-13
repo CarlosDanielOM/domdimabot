cbd52e0d01d363241f6748fc9ded1403b3e75b5b
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-03-13 01:01:01 -0700

    Update furrometro.js

--- commands/furrometro.js
+++ commands/furrometro.js
@@ -1,4 +1,4 @@
-let furrometro = new Map();
+let furrymeter = new Map();
 
 let supremeFurry = 0;
 
@@ -6,23 +6,23 @@ let furryDay = new Date().getDay();
 
 async function furrometro(channel, username, argument, userlevel) {
     if (furryDay != new Date().getDay()) {
-        furrometro = new Map();
+        furrymeter = new Map();
         supremeFurry = 0;
         furryDay = new Date().getDay();
     }
     if (argument == 'reset' && userlevel >= 6) {
-        furrometro = new Map();
+        furrymeter = new Map();
         supremeFurry = 0;
         return { error: false, message: `El furrometro ha sido reseteado por ${username}` };
     } else if (argument == 'reset' && userlevel < 6) {
         return { error: true, reason: 'No tienes permisos para resetear el furrometro' };
     }
 
-    if (furrometro.has(username.tolowerCase())) {
-        let furryValue = furrometro.get(username.tolowerCase());
+    if (furrymeter.has(username.tolowerCase())) {
+        let furryValue = furrymeter.get(username.tolowerCase());
         return { error: false, message: `El dia de hoy ${username} es ${furryValue}%` };
     }
-    
+
     let rand = Math.floor(Math.random() * 100) + 1;
 
     if (rand > supremeFurry) {
@@ -40,7 +40,7 @@ async function furrometro(channel, username, argument, userlevel) {
         });
     }
 
-    furrometro.set(username.tolowerCase(), rand);
+    furrymeter.set(username.tolowerCase(), rand);
 
     return { error: false, message: `${username} tiene un nivel de furro del ${rand}%` };
 
