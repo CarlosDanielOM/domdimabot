222a0fb5aab99016c80e009bf677d0e1a67efab9
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-03-17 22:40:51 -0700

    Update setmoderator.js

--- functions/channel/setmoderator.js
+++ functions/channel/setmoderator.js
@@ -1,10 +1,11 @@
 const { getStreamerHeader } = require('../../util/headers');
 const getUserID = require('../getuserid');
+const { getTwitchHelixURL } = require('../../util/links');
 
 async function setModerator(channel, user = 698614112) {
     const headers = await getStreamerHeader(channel);
     const userID = await getUserID(channel);
-    let response = await fetch(`${this.helixURL}/moderation/moderators?broadcaster_id=${userID}&user_id=${user}`, {
+    let response = await fetch(`${getTwitchHelixURL()}/moderation/moderators?broadcaster_id=${userID}&user_id=${user}`, {
         method: 'POST',
         headers: headers
     });
