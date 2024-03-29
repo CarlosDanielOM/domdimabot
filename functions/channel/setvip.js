a48c5e06ecfed3839f1e166dcc924907595ef551
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-03-28 23:18:03 -0700

    Update setvip.js

--- functions/channel/setvip.js
+++ functions/channel/setvip.js
@@ -4,6 +4,8 @@ const { getStreamerHeader } = require('../../util/headers')
 async function setVip(channelID, headers, userID) {
     const helixURL = getTwitchHelixURL();
 
+    console.log({ channelID, headers, userID, helix: `${helixURL}/channels/vips?broadcaster_id=${channelID}&user_id=${userID}`, where: 'setVip' })
+
     let setVIP = await fetch(`${helixURL}/channels/vips?broadcaster_id=${channelID}&user_id=${userID}`, { method: 'POST', headers: headers });
 
     if (setVIP.status == 204) return { error: false, status: 204 }
