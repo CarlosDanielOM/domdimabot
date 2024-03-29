44369616e0d58430823090bdd0467711e65fa701
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-03-28 23:09:40 -0700

    Issues test

--- redemption_functions/vip.js
+++ redemption_functions/vip.js
@@ -4,9 +4,12 @@ const { getStreamerHeader } = require('../util/headers');
 async function vipRedemtionFun(eventData, rewardData) {
     const { broadcaster_user_id, broadcaster_user_login, user_id } = eventData;
     let headers = await getStreamerHeader(broadcaster_user_login);
+    console.log({ headers, where: 'vipRedemtionFun' })
 
     if (rewardData.title === 'VIP') {
+        console.log('VIP');
         let result = await CHANNEL.setVIP(broadcaster_user_id, headers, user_id);
+        console.log({ result, where: 'vipRedemtionFun' })
         if (result.error) return { error: true, reason: result.message };
         if (result.status !== 204) return { error: true, reason: 'Error setting VIP' };
         return { error: false, message: 'VIP set' };
