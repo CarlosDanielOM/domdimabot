7e5f6ac32ce671731412bc38975fc49701689d29
CDOM201 <carlos@carlosdaniel.info>
2024-05-04 23:07:54 -0700

    EventSub CRUD
    Event sub api routes

--- src/server.js
+++ src/server.js
@@ -948,25 +948,68 @@ async function init() {
     res.status(200).json(data);
   });
 
-  app.get('/eventsubs/:channelID/:id', async (req, res) => {
-    const { channelID, id } = req.params;
+  app.get('/eventsubs/:id', async (req, res) => {
+    const { id } = req.params;
 
-    let data = await eventsubSchema.findOne({ channelID: channelID, _id: id });
+    let data = await eventsubSchema.findById(id);
 
     if(!data) return res.status(404).json({ message: 'Eventsub not found', error: true });
 
-    res.status(200).json({data, error: false});
+    res.status(200).json({eventsub: data, error: false});
+  });
+
+  app.get('/eventsubs/:channelID/:type', async (req, res) => {
+    const { channelID, type } = req.params;
+
+    let data = await eventsubSchema.findOne({ channelID: channelID, type: type });
+
+    if(!data) return res.status(404).json({ message: 'Eventsub not found', error: true });
+
+    res.status(200).json({eventsubs: data, error: false});
   });
 
   app.post('/eventsubs/:channelID', async (req, res) => {
     const { channelID } = req.params;
     const { type, version, condition } = req.body;
 
-    let streamer = await STREAMERS.getStreamer(channelID);
+    let streamer = await STREAMERS.getStreamerById(channelID);
+
+    if(!streamer) return res.status(404).json({ message: 'Streamer not found', error: true });
 
     let response = await subscribeTwitchEvent(streamer.name, type, version, condition);
 
     res.status(200).json(response);
+  });
+
+  app.delete('/eventsubs/:channelID/:id', async (req, res) => {
+    const { channelID, id } = req.params;
+
+    let eventsub = await eventsubSchema.findOne({ channelID: channelID, _id: id });
+
+    if(!eventsub) return res.status(404).json({ message: 'Eventsub not found', error: true });
+
+    let result = await unsubscribeTwitchEvent(eventsub.id);
+
+    if(result.error) return res.status(result.status).json(result);
+
+    return res.status(200).json({error: false, message: 'Eventsub deleted'});
+    
+  });
+
+  app.patch('/eventsubs/:channelID/:id', async (req, res) => {
+    const { channelID, id } = req.params;
+    let body = req.body;
+
+    let eventsub = await eventsubSchema.findOne({ channelID, _id: id});
+
+    if(!eventsub) return res.status(404).json({ message: 'Eventsub not found', error: true });
+
+    let update = await eventsubSchema.findOneAndUpdate({ channelID, _id: id}, body, { new: true });
+
+    if(!update) return res.status(400).json({ message: 'Error updating eventsub', error: true });
+
+    res.status(200).json({ message: 'Eventsub updated', error: false, eventsub: update });
+    
   });
 
   //? Server ?//
