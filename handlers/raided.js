8af029203c4838e61c6c9f9d3198089fd23a2445
CDOM201 <carlos@carlosdaniel.info>
2024-05-20 13:05:23 -0700

    Updates to Automatic SO

--- handlers/raided.js
+++ handlers/raided.js
@@ -8,12 +8,11 @@ async function raid(client, eventData, eventsubData) {
     if (eventsubData.minViewers > eventData.viewers) return;
     
     let message = await textConvertor(eventData.to_broadcaster_user_id, eventData, eventsubData.message);
-    console.log({message});
     
     let anoun = await announcement(eventData.to_broadcaster_user_id, modID, message.message, 'purple');
     
-    let { soClip } = await shoutout(eventData.from_broadcaster_user_login, eventData.to_broadcaster_user_login, modID);
-    if(!soClip) return client.say(eventData.to_broadcaster_user_login, "Error al reproducir el clip");
+    await shoutout(eventData.to_broadcaster_user_id, eventData.from_broadcaster_user_id, modID);
+    // if(!soClip) return client.say(eventData.to_broadcaster_user_login, "Error al reproducir el clip");
 }
 
 module.exports = raid;
